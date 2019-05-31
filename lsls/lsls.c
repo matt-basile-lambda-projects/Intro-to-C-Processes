22985a721d0e29cac89f9f80d1731935e93148f9
Matt Basile <matthewbasile2@gmail.com>
2019-05-31 11:18:39 -0400

    opendir and readdir logic in place for . need to make it dynamic

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  DIR *dir;
+  struct dirent *sd;
+
   // Parse command line
 
   // Open directory
+  dir = opendir(".");
+  if(dir == NULL){
+    printf("Error! Unable to open directory");
+    exit(1);
+  }
+  while( (sd=readdir(dir)) != NULL ){
+    printf(">> %s\n", sd->d_name);
+  }
+  
+
 
   // Repeatly read and print entries
 
   // Close directory
-
+  closedir(dir);
   return 0;
 }
