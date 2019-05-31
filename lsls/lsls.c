3f1db900a304f5905bbe20407aeafd03eab64db9
Matt Basile <matthewbasile2@gmail.com>
2019-05-31 11:37:10 -0400

    showing file size

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,25 +2,31 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
-
+#include <sys/stat.h>
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
   DIR *dir;
-  struct dirent *sd;
+  struct dirent *ent;
+  struct stat buf;
 
   // Parse command line
 
   // Open directory
   dir = opendir(".");
+  
   if(dir == NULL){
     printf("Error! Unable to open directory");
     exit(1);
   }
-  while( (sd=readdir(dir)) != NULL ){
-    printf(">> %s\n", sd->d_name);
+  while( (ent=readdir(dir)) != NULL ){
+    char *name = ent->d_name;
+    printf(">> %s\n", name);
+    // printf(">> %1llu\n", ent->d_ino);
+    stat(name, &buf);
+    printf("file size is %lld\n", buf.st_size);
   }
   
 
